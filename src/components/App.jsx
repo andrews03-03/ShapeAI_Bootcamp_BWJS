4a9d4a083abd47de7a0b20b5b0d4f455af54d948
andrews03-03 <85432991+andrews03-03@users.noreply.github.com>
2021-06-29 23:27:13 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import List from "./List";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <List />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List() {
+  return (
+    <div className="note">
+      <h2>Shape AI project</h2>
+      <p>Basic web development</p>
+    </div>
+  );
+}
+
+export default List;
